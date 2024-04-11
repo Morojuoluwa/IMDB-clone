bb70a4b673f6b7af3764261cb639d5424d09e376
Morojuoluwa <abimbolapatrickmj@gmail.com>
2024-04-11 05:01:26 -0700

    fatched data from tmdb api to the Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
 import React from 'react'
+const API_KEY = process.env.API_KEY
 
-const Home = () => {
+const Home = async({searchParams}) => {
+  const genre = searchParams.genre || 'fetchTrending'
+  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated'?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`)
+  const data = await res.json()
+  if(!res.ok){
+    throw new Error('something is probably wrong with the api')
+  }
+
+  const result = data?.results
+
+  console.log(result)
   return (
     <div>Home</div>
   )
